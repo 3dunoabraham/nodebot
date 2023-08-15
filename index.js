cbe09541050a58b5a902abcb3dea3ad50156759b
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-08-14 21:43:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function create_cron_datetime(seconds, minute, hour, day_of_the_month, month, da
 console.log(`we have begun; ${create_cron_datetime(0, 0, 0, 2, 0, 0)}`);
 
 cron.schedule(
-  create_cron_datetime("*/120", '*', '*', '*', '*', '*'),
+  create_cron_datetime("*/33", '*', '*', '*', '*', '*'),
   // create_cron_datetime("*/3", '*', '*', '*', '*', '*'),
   async function() {
     const playerHash = "71e0306864eb7e22c2fc5b77104b1f3196769ac72f22a4cd0dd87d10ed28d2b0";
