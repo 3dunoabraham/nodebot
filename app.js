d07c09a0c962e9f28dccef67b72187ade652ad48
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-08-16 00:14:38 -0400

    supabase

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+const { createClient } = require('@supabase/supabase-js');
 const express = require('express')
 const createError = require('http-errors')
 const morgan = require('morgan')
 require('dotenv').config()
 const { Telegraf } = require('telegraf');
+
+const hardcode = {
+  "c841339a5bafe0b116abc6dc4746557dc3cc9ebadc6f2d68423c1efd3cc34da7" : 3
+}
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome'));
@@ -11,12 +16,12 @@ bot.on('inline_query', async (ctx) => {
   const queryText = ctx.update.inline_query.query;
 
   const randdd = parseInt(Math.random() * 100)
-  const results = generateInlineResults(queryText,randdd);
+  const results = await generateInlineResults(queryText,randdd);
 
   await ctx.answerInlineQuery(results);
 });
 
-function generateInlineResults(queryText,randdd) {
+async function generateInlineResults(queryText,randdd) {
   const results = [];
   const textResult = {
     type: 'article',
@@ -26,8 +31,24 @@ function generateInlineResults(queryText,randdd) {
       message_text: `You entered: ${queryText} \nYou got: ${randdd}`,
     },
   };
-
-  results.push(textResult);
+  
+  // const foundHardcode = hardcode[queryText]
+  let thePllayer = null;
+  try {
+    thePllayer = await fetchPlayer(queryText)
+  } catch (error) {
+    thePllayer = {name:`foundHardcode: ${foundHardcode}`}
+    
+  }
+  const betterResult = !thePllayer.subscription ? textResult : {
+    type: 'article',
+    id: '1',
+    title: '->Text Result +++'+thePllayer.name,
+    input_message_content: {
+      message_text: `ey: ${thePllayer.name} ye You entered: |${queryText}| \n${queryText.length}You got: ${randdd}`,
+    },
+  };
+  results.push(betterResult);
 
   return results;
 }
@@ -35,6 +56,20 @@ bot.launch();
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+async function fetchPlayer(playerHash) {
+  const supabase = createClient(supabaseUrl, supabaseKey, { auth: { persistSession: false } });
+  const { data: existingStart, error: selectError } = await supabase
+    .from('player')
+    // .select()
+    .select('name, attempts, totalAttempts, goodAttempts, trades, mode, jwt, binancekeys, subscription, referral')
+    .match({ hash: playerHash })
+    .single();
+  return existingStart;
+}
 
 const app = express()
 app.use(express.json())
