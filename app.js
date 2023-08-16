18f7acbff0546ced922ed4c57ad047cebf12b243
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-08-15 22:26:53 -0400

    telegraf

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express')
 const createError = require('http-errors')
 const morgan = require('morgan')
 require('dotenv').config()
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.launch();
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 const app = express()
 app.use(express.json())
