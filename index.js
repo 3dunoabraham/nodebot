1ba8d57387d3e02dfadafcb294e5ddcce98aaeb7
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-08-15 21:36:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const { Telegraf } = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.hears('hi', (ctx) => ctx.reply('Hey hihi' + ` ${JSON.stringify(Object.keys(ctx.state))}`));
+// bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.on('channel_post', (ctx) => ctx.reply('Hey channel_post:' + `Hello ${JSON.stringify(Object.keys(ctx.telegram))}`))
+
 bot.launch();
 
 // Enable graceful stop
